50a48875ec17798d3bb57c41757e1f92740503a2
juniawativitri1 <juniawativitri1@gmail.com>
2022-09-19 17:17:57 +0700

    upload tugas1

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,9 @@
+<?php
+class Contoh1 extend CI_Controller
+{
+    public function index()
+    {
+echo "<h1>perkenalkan</h1>";
+echo"Nama saya Vitri Juniawati saya tinggal didaerah Desa Mekar Baru olahraga yang saya sukai bulutangkis";
+    }
+}
